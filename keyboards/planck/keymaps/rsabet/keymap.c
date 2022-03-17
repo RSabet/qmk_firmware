8458b2be826a9e8f356a1276b08fcad4d4316826
RSabet <ramin.sabet@gmail.com>
2022-03-17 22:22:01 +0100

    trying qwertz

--- keyboards/planck/keymaps/rsabet/keymap.c
+++ keyboards/planck/keymaps/rsabet/keymap.c
@@ -50,18 +50,15 @@ enum planck_layers {
   _QWERTZ,    // 8
 };
 
+enum tap_dance_codes {
+  DANCE_0,
+  DANCE_1,
+  DANCE_2,
+};
+
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
-        /* ┌──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┬──────┐
-         * │   ~  │   1  │   2  │   3  │   4  │   5  │   6  │   7  │   8  │   9  │   0  │ DEL  │
-         * ├──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┤
-         * │   `  │   !  │   @  │   #  │   $  │   %  │   ^  │   &  │   *  │   ü  │   ö  │   ä  │
-         * ├──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┤
-         * │ SINS │   …  │   ’  │   “  │   ”  │   ←  │   →  │   €  │   ß  │   Ü  │   Ö  │   Ä  │
-         * ├──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┼──────┤
-         * │ Rset │ BLSt │  F1  │ Home │ End  │ PgUp │ PgDn │ Left │ Down │  Up  │ Rght │      │
-         * └──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┴──────┘
-         */
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_BASE] = LAYOUT_planck_grid(
@@ -131,9 +128,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_NAVNUM] = LAYOUT_planck_grid(
     //┌─────────┬─────────┬─────────┬─────────┬─────────┬─────────┐ ┌─────────┬─────────┬─────────┬─────────┬─────────┬─────────┐
-        _______ , KC_HOME , KC_DEL  , KC_UP,      KC_BSPACE,   KC_PGUP,     DE_SLSH,    KC_7,     KC_8,    KC_9,    DE_MINS, KC_DEL,
-        _______ , KC_END  , KC_LEFT ,   KC_DOWN,    KC_RIGHT,    KC_PGDOWN,   DE_ASTR,    KC_4,     KC_5,    KC_6,    DE_PLUS,  DE_COLN,
-        _______ , _______ , KC_PC_CUT, KC_PC_COPY, KC_PC_PASTE, S(DE_CIRC) , KC_0,       KC_1,     KC_2,    KC_3,    KC_COMMA, KC_DOT,
+        _______ , KC_HOME , KC_DEL  , KC_UP   ,KC_BSPACE, KC_PGUP ,   DE_SLSH , KC_7    , KC_8    , KC_9    , DE_MINS , KC_DEL  ,
+    //├─────────┼─────────┼─────────┼─────────┼─────────┼─────────┤ ├─────────┼─────────┼─────────┼─────────┼─────────┼─────────┤
+        _______ , KC_END  , KC_LEFT , KC_DOWN , KC_RIGHT,KC_PGDOWN,   DE_ASTR , KC_4    , KC_5    , KC_6    , DE_PLUS , DE_COLN ,
+    //├─────────┼─────────┼─────────┼─────────┼─────────┼─────────┤ ├─────────┼─────────┼─────────┼─────────┼─────────┼─────────┤
+        _______ , _______ ,KC_PC_CUT,KC_PC_COPY, KC_PC_PASTE, S(DE_CIRC) , KC_0,       KC_1,     KC_2,    KC_3,    KC_COMMA, KC_DOT,
+    //├─────────┼─────────┼─────────┼─────────┼─────────┼─────────┤ ├─────────┼─────────┼─────────┼─────────┼─────────┼─────────┤
         _______ , _______ , _______,   _______,    _______,     _______,     KC_NO,      _______,  KC_0,    _______, _______,  _______
   ),
 
@@ -151,9 +151,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTZ] = LAYOUT_planck_grid(
     //┌─────────┬─────────┬─────────┬─────────┬─────────┬─────────┐ ┌─────────┬─────────┬─────────┬─────────┬─────────┬─────────┐
-        _______ , KC_Q    , KC_W    , KC_E    , KC_R    , KC_T    ,   DE_Z    , KC_U    , KC_I    , KC_O    , KC_P    , _______ ,
+       _______ , KC_Q    , KC_W    , KC_E    , KC_R    , KC_T    ,   DE_Z    , KC_U    , KC_I    , KC_O    , KC_P    , _______ ,
     //├─────────┼─────────┼─────────┼─────────┼─────────┼─────────┤ ├─────────┼─────────┼─────────┼─────────┼─────────┼─────────┤
-        _______ , KC_A    , KC_S    , KC_D    , KC_F    , KC_G    ,   KC_H    , KC_J    , KC_K    , KC_L    , DE_ODIA , DE_ADIA ,
+       LT(_SYMBOLS_R,KC_ESCAPE) , KC_A    , KC_S    , KC_D    , KC_F    , KC_G    ,   KC_H    , KC_J    , KC_K    , KC_L    , DE_ODIA , DE_ADIA ,
     //├─────────┼─────────┼─────────┼─────────┼─────────┼─────────┤ ├─────────┼─────────┼─────────┼─────────┼─────────┼─────────┤
         _______ , DE_Y    , KC_X    , KC_C    , KC_V    , KC_B    ,   KC_N    , KC_M    , _______ , _______ , DE_UDIA , _______ ,
     //├─────────┼─────────┼─────────┼─────────┼─────────┼─────────┤ ├─────────┼─────────┼─────────┼─────────┼─────────┼─────────┤
@@ -500,3 +500,143 @@ const key_override_t **key_overrides = (const key_override_t *[]){
     &delete_key_override,
     NULL
 };
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+/* typedef struct {
+    bool is_press_action;
+    uint8_t step;
+} tap;
+
+enum {
+    SINGLE_TAP = 1,
+    SINGLE_HOLD,
+    DOUBLE_TAP,
+    DOUBLE_HOLD,
+    DOUBLE_SINGLE_TAP,
+    MORE_TAPS
+};
+
+static tap dance_state[3];
+
+uint8_t dance_step(qk_tap_dance_state_t *state);
+
+uint8_t dance_step(qk_tap_dance_state_t *state) {
+    if (state->count == 1) {
+        if (state->interrupted || !state->pressed) return SINGLE_TAP;
+        else return SINGLE_HOLD;
+    } else if (state->count == 2) {
+        if (state->interrupted) return DOUBLE_SINGLE_TAP;
+        else if (state->pressed) return DOUBLE_HOLD;
+        else return DOUBLE_TAP;
+    }
+    return MORE_TAPS;
+}
+
+
+void on_dance_0(qk_tap_dance_state_t *state, void *user_data);
+void dance_0_finished(qk_tap_dance_state_t *state, void *user_data);
+void dance_0_reset(qk_tap_dance_state_t *state, void *user_data);
+
+void on_dance_0(qk_tap_dance_state_t *state, void *user_data) {
+    if(state->count == 3) {
+        tap_code16(KC_TAB);
+        tap_code16(KC_TAB);
+        tap_code16(KC_TAB);
+    }
+    if(state->count > 3) {
+        tap_code16(KC_TAB);
+    }
+}
+
+void dance_0_finished(qk_tap_dance_state_t *state, void *user_data) {
+    dance_state[0].step = dance_step(state);
+    switch (dance_state[0].step) {
+        case SINGLE_TAP: register_code16(KC_TAB); break;
+        case SINGLE_HOLD: layer_move(7); break;
+        case DOUBLE_TAP: register_code16(KC_TAB); register_code16(KC_TAB); break;
+        case DOUBLE_SINGLE_TAP: tap_code16(KC_TAB); register_code16(KC_TAB);
+    }
+}
+
+void dance_0_reset(qk_tap_dance_state_t *state, void *user_data) {
+    wait_ms(10);
+    switch (dance_state[0].step) {
+        case SINGLE_TAP: unregister_code16(KC_TAB); break;
+        case DOUBLE_TAP: unregister_code16(KC_TAB); break;
+        case DOUBLE_SINGLE_TAP: unregister_code16(KC_TAB); break;
+    }
+    dance_state[0].step = 0;
+}
+void on_dance_1(qk_tap_dance_state_t *state, void *user_data);
+void dance_1_finished(qk_tap_dance_state_t *state, void *user_data);
+void dance_1_reset(qk_tap_dance_state_t *state, void *user_data);
+
+void on_dance_1(qk_tap_dance_state_t *state, void *user_data) {
+    if(state->count == 3) {
+        tap_code16(KC_ESCAPE);
+        tap_code16(KC_ESCAPE);
+        tap_code16(KC_ESCAPE);
+    }
+    if(state->count > 3) {
+        tap_code16(KC_ESCAPE);
+    }
+}
+
+void dance_1_finished(qk_tap_dance_state_t *state, void *user_data) {
+    dance_state[1].step = dance_step(state);
+    switch (dance_state[1].step) {
+        case SINGLE_TAP: register_code16(KC_ESCAPE); break;
+        case SINGLE_HOLD: layer_move(4); break;
+        case DOUBLE_TAP: register_code16(KC_ESCAPE); register_code16(KC_ESCAPE); break;
+        case DOUBLE_SINGLE_TAP: tap_code16(KC_ESCAPE); register_code16(KC_ESCAPE);
+    }
+}
+
+void dance_1_reset(qk_tap_dance_state_t *state, void *user_data) {
+    wait_ms(10);
+    switch (dance_state[1].step) {
+        case SINGLE_TAP: unregister_code16(KC_ESCAPE); break;
+        case DOUBLE_TAP: unregister_code16(KC_ESCAPE); break;
+        case DOUBLE_SINGLE_TAP: unregister_code16(KC_ESCAPE); break;
+    }
+    dance_state[1].step = 0;
+}
+void on_dance_2(qk_tap_dance_state_t *state, void *user_data);
+void dance_2_finished(qk_tap_dance_state_t *state, void *user_data);
+void dance_2_reset(qk_tap_dance_state_t *state, void *user_data);
+
+void on_dance_2(qk_tap_dance_state_t *state, void *user_data) {
+    if(state->count == 3) {
+        tap_code16(DE_ADIA);
+        tap_code16(DE_ADIA);
+        tap_code16(DE_ADIA);
+    }
+    if(state->count > 3) {
+        tap_code16(DE_ADIA);
+    }
+}
+
+void dance_2_finished(qk_tap_dance_state_t *state, void *user_data) {
+    dance_state[2].step = dance_step(state);
+    switch (dance_state[2].step) {
+        case SINGLE_TAP: register_code16(DE_ADIA); break;
+        case SINGLE_HOLD: layer_move(5); break;
+        case DOUBLE_TAP: register_code16(DE_ADIA); register_code16(DE_ADIA); break;
+        case DOUBLE_SINGLE_TAP: tap_code16(DE_ADIA); register_code16(DE_ADIA);
+    }
+}
+
+void dance_2_reset(qk_tap_dance_state_t *state, void *user_data) {
+    wait_ms(10);
+    switch (dance_state[2].step) {
+        case SINGLE_TAP: unregister_code16(DE_ADIA); break;
+        case DOUBLE_TAP: unregister_code16(DE_ADIA); break;
+        case DOUBLE_SINGLE_TAP: unregister_code16(DE_ADIA); break;
+    }
+    dance_state[2].step = 0;
+}
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+        [DANCE_0] = ACTION_TAP_DANCE_FN_ADVANCED(on_dance_0, dance_0_finished, dance_0_reset),
+        [DANCE_1] = ACTION_TAP_DANCE_FN_ADVANCED(on_dance_1, dance_1_finished, dance_1_reset),
+        [DANCE_2] = ACTION_TAP_DANCE_FN_ADVANCED(on_dance_2, dance_2_finished, dance_2_reset),
+}; */
